fb4f727730b165a97e8c678608e3ebfac8d8637c
Hamid Q <hamid.qureshi@gmail.com>
2021-01-01 00:23:23 +1100

    Updates to code and load of data
    Updates to code and load of data

--- ch2/r3/01_manage_table.sql
+++ ch2/r3/01_manage_table.sql
@@ -1,34 +1,41 @@
 -- create a table 
 CREATE TABLE customers (
   id              INT NOT NULL,
-  last_name       VARCHAR(50) ,
-  first_name      VARCHAR(50),
-  email           VARCHAR(50),
-  company         VARCHAR(50),
-  phone           VARCHAR(25),
+  last_name       VARCHAR(100) ,
+  first_name      VARCHAR(100),
+  email           VARCHAR(100),
+  company         VARCHAR(100),
+  phone           VARCHAR(100),
   address1        VARCHAR(150),
   address2        VARCHAR(150),
-  city            VARCHAR(50),
-  state           VARCHAR(50),
+  city            VARCHAR(100),
+  state           VARCHAR(100),
   postal_code     VARCHAR(15),
   country         VARCHAR(50)
 );
 
+
 -- replace the table
 CREATE OR REPLACE TABLE customers (
   id              INT NOT NULL,
-  last_name       VARCHAR(50) ,
-  first_name      VARCHAR(50),
-  email           VARCHAR(50),
-  company         VARCHAR(50),
-  phone           VARCHAR(25),
+  last_name       VARCHAR(100) ,
+  first_name      VARCHAR(100),
+  email           VARCHAR(100),
+  company         VARCHAR(100),
+  phone           VARCHAR(100),
   address1        STRING,
   address2        STRING,
-  city            VARCHAR(50),
-  state           VARCHAR(50),
+  city            VARCHAR(100),
+  state           VARCHAR(100),
   postal_code     VARCHAR(15),
   country         VARCHAR(50)
 );
+
+-- load sample data
+COPY INTO customers
+FROM s3://snowflake-cookbook/ch2/r3/customer.csv
+FILE_FORMAT = (TYPE = csv SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"');
+
 
 -- replace table selecting all data from the customer table
 CREATE OR REPLACE TABLE 
@@ -48,3 +55,9 @@ SELECT
 COUNT(*) shallow_count 
 FROM 
 customers_shallow_copy;
+
+-- create a temporary table
+CREATE TEMPORARY TABLE customers_temp AS SELECT * FROM customers WHERE TRY_TO_NUMBER(postal_code) IS NOT NULL;
+
+-- create a transient table
+CREATE TRANSIENT TABLE customers_trans AS AS SELECT * FROM customers WHERE TRY_TO_NUMBER(postal_code) IS NULL;
