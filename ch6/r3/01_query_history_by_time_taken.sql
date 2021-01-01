9c61ab9b78e896c5470c5806a2abd834e5052448
Hamid Q <hamid.qureshi@gmail.com>
2021-01-01 18:29:52 +1100

    Adding ACCOUNTADMIN role
    Adding ACCOUNTADMIN role

--- ch6/r3/01_query_history_by_time_taken.sql
+++ ch6/r3/01_query_history_by_time_taken.sql
@@ -1,10 +1,7 @@
 
 -- list queries ordered by most time taken for execution
+USE ROLE ACCOUNTADMIN;
 USE SNOWFLAKE;
 SELECT QUERY_ID, QUERY_TEXT, EXECUTION_TIME,USER_NAME 
 FROM SNOWFLAKE.ACCOUNT_USAGE.query_history 
 ORDER BY EXECUTION_TIME DESC;
-
-
--- use the query id to view query profiles
--- Use the Snowflake WebUI
